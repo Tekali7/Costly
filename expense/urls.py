e97e71dcdda23cb10deea858dcadf380da99b8c6
Alihan Teker <alihan.teker@hotmail.com>
2024-04-21 13:51:47 +0000

    Add 'add_expense' template with form

--- expense/urls.py
+++ expense/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("expense/", views.index, name="index")
+    path("expense/", views.index, name="index"),
+    path("add_expense/", views.add_expense, name="add_expense"),
+   # path("delete_expense/", views.delete_expense, name="delete_expense"),
+   # path("edit_expense/", views.edit_expense, name="edit_expense")
 ]
--- expense/views.py
+++ expense/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django import forms
 from .models import Expense
 
 # Create your views here.
@@ -13,3 +14,26 @@ def index(request):
     "expense/index.html", {
     "expenses": expenses
     })
+
+
+class ExpenseForm(forms.ModelForm):
+    class Meta:
+        model = Expense
+        fields = ['item', 'amount', 'currency']
+
+
+def add_expense(request):
+    if request.method == 'POST':
+        form = forms.Form(request.POST)
+        if form.is_valid():
+            item = form.cleaned_data['item']
+            amount = form.cleaned_data['amount']
+            currency = form.cleaned_data['currency']
+            if item and amount and currency:
+                expense = Expense.objects.create(item=item, amount=amount, currency=currency, user=request.user)
+                return redirect('expense')
+            else:
+                form.add_error(None, 'All fields are required.')
+    else:
+        form = ExpenseForm()
+    return render(request, 'expense/add_expense.html', {'form': form})
