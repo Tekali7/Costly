caa3027072e23af78249b3ec7a1d58796076b07a
Alihan Teker <alihan.teker@hotmail.com>
2024-04-25 10:15:52 +0000

    Add docstring to script

--- static/js/expense.js
+++ static/js/expense.js
@@ -1,3 +1,11 @@
+/**
+ * Expense deletion script.
+ * 
+ * Handles the deletion of expenses in the application.
+ * Creates a modal for confirming the deletion choice for better UX
+ * and dynamically updates the form action URL based on the expense ID.
+ */
+
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("delete-expense");
 const deleteForm = document.getElementById("deleteForm");
