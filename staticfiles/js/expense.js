5427dfe6c6b221194229493a4f7a55271659cec8
Alihan Teker <alihan.teker@hotmail.com>
2024-04-25 15:48:42 +0000

    Improve mobile styling

--- staticfiles/js/expense.js
+++ staticfiles/js/expense.js
@@ -1,13 +1,20 @@
+/**
+ * Expense deletion script.
+ * 
+ * Handles the deletion of expenses in the application.
+ * Creates a modal for confirming the deletion choice for better UX
+ * and dynamically updates the form action URL based on the expense ID.
+ */
+
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
 const deleteButtons = document.getElementsByClassName("delete-expense");
 const deleteForm = document.getElementById("deleteForm");
 const deleteConfirmButton = document.getElementById("deleteConfirm");
 
-// Add event listener to each delete button
+
 for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
         let expenseId = e.target.getAttribute("data-expense-id");
-        // Set the action attribute of the delete form
         deleteForm.action = `/delete_expense/${expenseId}`;
         deleteModal.show();
     });
